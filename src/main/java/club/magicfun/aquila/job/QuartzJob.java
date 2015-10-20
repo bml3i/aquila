ba23183db85c49abb72190d1905c2441201735fa
Leo Bi (Mac Pro) <biminglei@gmail.com>
2015-10-20 23:00:43 +0800

    test selenium

--- src/main/java/club/magicfun/aquila/job/QuartzJob.java
+++ src/main/java/club/magicfun/aquila/job/QuartzJob.java
@@ -1,5 +1,8 @@
 package club.magicfun.aquila.job;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,17 +17,32 @@ public class QuartzJob  {
 		System.out.println("Quartzjob创建成功");
 	}
 	
-	@Scheduled(cron = "0/30 * *  * * ? ")
+	@Scheduled(cron = "0/15 * * * * ? ")
     public void run(){
         System.out.println("Quartz执行的任务调度");
         
         WebDriver webDriver = new ChromeDriver();
         
-        //webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        
         webDriver.get("https://item.taobao.com/item.htm?id=43462858898"); 
-  
+        
+        //webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        
         System.out.println("title: " + webDriver.getTitle());
+  
+        List<WebElement> colorCategoryList = webDriver.findElements(By.xpath("//*[@data-property='颜色分类']/li/a"));
+        
+        for (WebElement colorCategory : colorCategoryList) {
+        	
+        	// simulate choosing a color
+        	colorCategory.click();
+        	
+        	WebElement selectedColorLi = webDriver.findElement(By.xpath("//*[contains(concat(' ', normalize-space(@class), ' '), ' tb-selected ')]"));
+        	System.out.println("selectedLi: " + selectedColorLi.findElements(By.cssSelector("a > span")).get(0).getAttribute("innerHTML"));
+        	
+        	System.out.println("price: " + webDriver.findElement(By.id("J_PromoPriceNum")).getText());
+        	System.out.println("stock: " + webDriver.findElement(By.id("J_SpanStock")).getText());
+        	
+        }
         
         webDriver.close();  
         webDriver.quit();
