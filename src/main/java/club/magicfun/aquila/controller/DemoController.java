a92ea6e4480acf2789bce83ffbf78b1a9db14887
Leo Bi (Mac Pro) <biminglei@gmail.com>
2015-12-15 21:13:19 +0800

    testing

--- src/main/java/club/magicfun/aquila/controller/DemoController.java
+++ src/main/java/club/magicfun/aquila/controller/DemoController.java
@@ -1,5 +1,8 @@
 package club.magicfun.aquila.controller;
 
+import java.util.List;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +12,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import club.magicfun.aquila.service.AuthorityService;
+import club.magicfun.aquila.model.Category;
+import club.magicfun.aquila.model.Product;
+import club.magicfun.aquila.repository.CategoryRepository;
+import club.magicfun.aquila.repository.ProductRepository;
 
 @Controller
 public class DemoController extends BaseController {
@@ -20,10 +26,28 @@ public class DemoController extends BaseController {
 	@Autowired
 	MessageSource messageSource;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@RequestMapping(value = "demo", method = RequestMethod.GET)
 	public String indexHandler(Model model) {
 		
+		List<Product> products = productRepository.findAll();
 		
+		if (products != null && products.size() > 0) {
+			for(Product product : products) {
+				logger.info(product.getId() + " - " + product.getProductName());
+				
+				Set<Category> categories = product.getCategories();
+				
+				for (Category category : categories) {
+					logger.info(category.getCategoryName() + " - " + category.getCategoryPrice() + " - " + category.getCategoryStockNumber());
+				}
+			}
+		}
 		
 		return "demo/index";
 	}
