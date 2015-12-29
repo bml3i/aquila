0b295751503d9d4b73abf94263dc62244212492d
Leo Bi (Mac Pro) <biminglei@gmail.com>
2015-12-29 22:53:03 +0800

    testing

---
+++ src/main/java/club/magicfun/aquila/job/TestWebDriver4.java
@@ -0,0 +1,90 @@
+package club.magicfun.aquila.job;
+
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TestWebDriver4 {
+
+	public static void main(String[] args) {
+
+		String dir = System.getProperty("user.dir");
+		String osName = System.getProperty("os.name");
+		
+		// load the appropriate chrome drivers according to the OS type
+		if (osName.toLowerCase().indexOf("windows") > -1) {
+			System.setProperty("webdriver.chrome.driver", dir + File.separator + "drivers" + File.separator + "win"
+					+ File.separator + "chromedriver.exe");
+		} else if (osName.toLowerCase().indexOf("mac") > -1) {
+			System.setProperty("webdriver.chrome.driver", dir + File.separator + "drivers" + File.separator + "mac"
+					+ File.separator + "chromedriver");
+		}
+		
+		Long productId = 522818128551l;
+		
+		WebDriver webDriver = new ChromeDriver();
+        
+        webDriver.get("https://item.taobao.com/item.htm?id=" + productId);
+        
+        System.out.println("title: " + webDriver.getTitle());
+        
+        
+        WebElement dealCountButton = webDriver.findElement(By.xpath("//a[@shortcut-label='查看成交记录查看成交记录']"));
+        
+        System.out.println("dealCountButton Location: " + dealCountButton.getLocation());
+        
+        dealCountButton.click();
+        
+        webDriver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS); 
+        
+        List<WebElement> dealRecordElements = webDriver.findElements(By.xpath("//div[@id='deal-record']/table/tbody/tr"));
+        
+        for (WebElement dealRecord : dealRecordElements) {
+        	//System.out.println("dealRecord Location: " + dealRecord.getLocation());
+        	System.out.println(dealRecord.findElement(By.xpath("td[@class='tb-start']")).getText().trim());
+        }
+        
+        
+        
+        
+  /*
+        List<WebElement> colorCategoryList = webDriver.findElements(By.xpath("//*[@data-property='颜色分类']/li/a"));
+        
+        for (WebElement colorCategory : colorCategoryList) {
+        	
+        	// simulate choosing a color
+        	System.out.println("colorCategory Location: " + colorCategory.getLocation());
+        	colorCategory.click();
+        	
+        	//webDriver.manage().timeouts().implicitlyWait(500, TimeUnit.MICROSECONDS); 
+        	
+        	WebElement selectedColorLi = webDriver.findElement(By.xpath("//*[contains(concat(' ', normalize-space(@class), ' '), ' tb-selected ')]"));
+        	System.out.println("selectedLi: " + selectedColorLi.findElements(By.cssSelector("a > span")).get(0).getAttribute("innerHTML"));
+        	
+        	WebElement testElement = webDriver.findElement(By.id("J_PromoPriceNum"));
+        	
+        	System.out.println("testElement Location: " + testElement.getLocation());
+        	
+        	System.out.println("price: " + webDriver.findElement(By.id("J_PromoPriceNum")).getText());
+        	System.out.println("stock: " + webDriver.findElement(By.id("J_SpanStock")).getText());
+        	
+        	System.out.print("\n");
+        	
+        }
+  */      
+        
+        webDriver.close();  
+        webDriver.quit();
+        
+		
+	}
+
+}
