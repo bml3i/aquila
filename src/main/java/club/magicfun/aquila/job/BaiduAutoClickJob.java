ed32856afbc9c823eded3be04e39577d83cebfe4
Leo <biminglei@gmail.com>
2016-01-05 12:51:43 +0800

    code changes

--- src/main/java/club/magicfun/aquila/job/BaiduAutoClickJob.java
+++ src/main/java/club/magicfun/aquila/job/BaiduAutoClickJob.java
@@ -32,7 +32,7 @@ public class BaiduAutoClickJob {
 	
 	private static final Logger logger = LoggerFactory.getLogger(BaiduAutoClickJob.class);
 	
-	private static final String BAIDU_SEARCH_URL = "http://www.baidu.com/s?wd={KEYWORD}&rsv_iqid＝{RANDOM}"; 
+	private static final String BAIDU_SEARCH_URL = "http://www.baidu.com/s?wd={KEYWORD}&rsv_iqid-{RANDOM}"; 
 	
 	private static final int AGENT_NUMBER_PER_TIME = 5; 
 	
@@ -42,7 +42,11 @@ public class BaiduAutoClickJob {
 	//private static final String[] targetLinkPartialTexts = {"吕记包子速冻包子 知名品牌速冻包子", "包子加盟_包子连锁_灌汤包加盟-天津吕记包子加盟连锁", "包子培训汤包加盟灌汤包加盟狗不"};
 	
 	private static final String searchKeyword = "包子培训";
-	private static final String[] targetLinkPartialTexts = {"天津老卜家汤包", "老卜家汤包", "汤包培训班老卜家汤包-历史传承品牌"};
+	private static final String[] targetLinkPartialTexts = {"天津包子培训基地", "大帅包"};
+	
+	
+	//private static final String searchKeyword = "包子培训";
+	//private static final String[] targetLinkPartialTexts = {"天津老卜家汤包", "老卜家汤包", "汤包培训班老卜家汤包-历史传承品牌"};
 	
 	public static final long SLEEP_TIME = 1000l; 
 	
