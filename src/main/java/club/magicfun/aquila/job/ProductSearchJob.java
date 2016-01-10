4ae1a8fd1a4a9602e4984b02b9286ca8fe60d203
Leo Bi (Mac Pro) <biminglei@gmail.com>
2016-01-10 22:13:24 +0800

    code changes

--- src/main/java/club/magicfun/aquila/job/ProductSearchJob.java
+++ src/main/java/club/magicfun/aquila/job/ProductSearchJob.java
@@ -377,7 +377,7 @@ public class ProductSearchJob {
 			
 			job = scheduleService.completeJob(job);
 		} else {
-			logger.warn("Job has not been configured for " + className + " or the job is not ready to run.");
+			logger.info("Job has not been configured for " + className + " or the job is not ready to run.");
 		}
 	}
 	
